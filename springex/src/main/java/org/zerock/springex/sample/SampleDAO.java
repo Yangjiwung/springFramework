7b3315900524272b0b69a9bdf0c56ad58737863d
Yangjiwung <155079266+Yangjiwung@users.noreply.github.com>
2024-06-05 03:14:07 +0900

    Add files via upload
    git upload

---
+++ springex/src/main/java/org/zerock/springex/sample/SampleDAO.java
@@ -0,0 +1,10 @@
+package org.zerock.springex.sample;
+
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ToString
+public class SampleDAO {
+
+    @Autowired SampleDAO sampleDAO;
+}
---
+++ springex/src/main/java/org/zerock/springex/sample/SampleService.java
@@ -0,0 +1,7 @@
+package org.zerock.springex.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+}
